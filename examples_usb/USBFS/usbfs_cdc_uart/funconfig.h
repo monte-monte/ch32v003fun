26d7ddc05b95cd571818b2485efad176f0c12aba
monte-monte <6649967+monte-monte@users.noreply.github.com>
2025-08-02 14:59:06 +0300

    Fix config for CH32V208 in USB CDC example

--- examples_usb/USBFS/usbfs_cdc_uart/funconfig.h
+++ examples_usb/USBFS/usbfs_cdc_uart/funconfig.h
@@ -2,7 +2,12 @@
 #define _FUNCONFIG_H
 
 
-// #define FUNCONF_USE_HSE           1
+
+#ifdef CH32V20x_D8W
+#define FUNCONF_USE_HSE             1
+#define FUNCONF_SYSTEM_CORE_CLOCK   120000000
+#define FUNCONF_PLL_MULTIPLIER      15
+#endif
 #define FUNCONF_DEBUGPRINTF_TIMEOUT 0x800000
 #define FUNCONF_ENABLE_HPE          0
 #define FUNCONF_SYSTICK_USE_HCLK    1
