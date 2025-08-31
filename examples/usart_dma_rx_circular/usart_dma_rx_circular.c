d9445106df58a082f4bf1acad8823de72f8734a0
Outfisken <matter.of.time2011@gmail.com>
2025-08-31 20:57:18 +0300

    Apply review comments and fixes

--- examples/usart_dma_rx_circular/usart_dma_rx_circular.c
+++ examples/usart_dma_rx_circular/usart_dma_rx_circular.c
@@ -1,15 +1,16 @@
+// This example demonstrates how to set up a simple command-processing loop using UART's RX DMA capability.
+// Using any terminal, send "toggle\r\n" to control the LED connected to PC7.
+
 #include "ch32fun.h"
-#include <stdio.h>
 
-#define RX_BUF_LEN 16
-#define CMD_BUF_LEN 16
+#define RX_BUF_LEN 16 // size of receive circular buffer
 
-u8 rx_buf[RX_BUF_LEN] = {0};
-u8 cmd_buf[CMD_BUF_LEN] = {0};
+u8 rx_buf[RX_BUF_LEN] = {0}; // DMA receive buffer for incoming data
+u8 cmd_buf[RX_BUF_LEN] = {0}; // buffer for complete command strings
 
-void process_cmd(u8* buf, u8 len)
+void process_cmd(u8* buf)
 {
-	if ( strstr((char*)buf, "toggle") != 0 ) {
+	if ( strncmp(buf, "toggle\r\n", 9) == 0) {
 		GPIOC->OUTDR ^= (1<<7);
 		printf("Horay!\r\n");
 	} else {
@@ -28,56 +29,71 @@ int main()
 	// enable rx pin
 	USART1->CTLR1 |= USART_CTLR1_RE;
 
-	// enable usart dma rx requests
+	// enable usart's dma rx requests
 	USART1->CTLR3 |= USART_CTLR3_DMAR;
 
 	// enable dma clock
 	RCC->AHBPCENR |= RCC_DMA1EN;
 
-	// configure dma
-	DMA1_Channel5->CFGR |=
-		DMA_MemoryDataSize_Byte | 
-		DMA_PeripheralDataSize_Byte |
-		DMA_MemoryInc_Enable |
-		DMA_Mode_Circular |
-		DMA_DIR_PeripheralSRC |
-		DMA_Priority_High;
-	DMA1_Channel5->CNTR = RX_BUF_LEN;
+	// configure dma for UART reception, it should fire on RXNE
 	DMA1_Channel5->MADDR = (u32)&rx_buf;
 	DMA1_Channel5->PADDR = (u32)&USART1->DATAR;
-	DMA1_Channel5->CFGR |= DMA_CFGR1_EN;
+	DMA1_Channel5->CNTR = RX_BUF_LEN;
+
+	// MEM2MEM: 0 (memory to peripheral)
+	// PL: 0 (low priority since UART is a relatively slow peripheral)
+	// MSIZE/PSIZE: 0 (8-bit)
+	// MINC: 1 (increase memory address)
+	// PINC: 0 (peripheral address remains unchanged)
+	// CIRC: 1 (circular)
+	// DIR: 0 (read from peripheral)
+	// TEIE: 0 (no tx error interrupt)
+	// HTIE: 0 (no half tx interrupt)
+	// TCIE: 0 (no transmission complete interrupt)
+	// EN: 1 (enable DMA)
+	DMA1_Channel5->CFGR = DMA_CFGR1_CIRC | DMA_CFGR1_MINC | DMA_CFGR1_EN;
+
 
 	while(1)
 	{
-		static u32 tail = 0;
-		static u32 head = 0;
-		static u32 cmd_st = 0;
-		static u32 cmd_end = 0;
+		static u32 tail = 0; // current read position in rx_buf
+		static u32 cmd_end = 0; // end index of current command in rx_buf
+		static u32 cmd_st = 0; // start index of current command in rx_buf
 
-		head = RX_BUF_LEN - DMA1_Channel5->CNTR;
-		while ( tail != head )
+		// calculate head position based on DMA counter (modulo when DMA1_Channel5->CNTR = 0)
+		u32 head = (RX_BUF_LEN - DMA1_Channel5->CNTR) % RX_BUF_LEN; // current write position in rx_buf
+		
+		// process new bytes in rx_buf. when a newline character is detected, the command is copied to cmd_buf
+		while (tail != head)
 		{
-			if (rx_buf[tail] == '\n')
+			if ( rx_buf[tail] == '\n' ) 
 			{
 				cmd_end = tail;
-				// to make life easier using string.h
-				rx_buf[tail]= '\0';
+				u32 cmd_i = 0; // carret position in cmd_buf
+				if (cmd_end > cmd_st)
+				{
+					for (u32 rx_i = cmd_st; rx_i < cmd_end + 1; rx_i++, cmd_i++) {
+						cmd_buf[cmd_i] = rx_buf[rx_i];
+					}
+				} else if (cmd_st > cmd_end) { // handle wrap around
+					for (u32 rx_i = cmd_st; rx_i < RX_BUF_LEN; rx_i++, cmd_i++) {
+						cmd_buf[cmd_i] = rx_buf[rx_i];
+					}
+					for (u32 rx_i = 0; rx_i < cmd_end + 1; rx_i++, cmd_i++) {
+						cmd_buf[cmd_i] = rx_buf[rx_i];
+					}
+				}
 
-				if (tail >= cmd_st)
-				{
-					memcpy(cmd_buf, &rx_buf[cmd_st], cmd_end+1);
-					process_cmd(cmd_buf, cmd_end+1);
-					cmd_st = (cmd_end + 1) % RX_BUF_LEN;
-				}
-				else // handle overlap
-				{
-					u8 st_len = RX_BUF_LEN - cmd_st;
-					memcpy(cmd_buf, &rx_buf[cmd_st], st_len);
-					memcpy(cmd_buf+st_len, &rx_buf[0], cmd_end+1);
-					process_cmd(cmd_buf, st_len + cmd_end + 1);
-					cmd_st = (cmd_end + 1) % RX_BUF_LEN;
-				}
+				// null terminate
+				cmd_buf[cmd_i] = '\0';
+
+				process_cmd(cmd_buf);
+
+				// update start position for next command
+				cmd_st = (cmd_end + 1) % RX_BUF_LEN;
 			}
+
+			// move to next position 
 			tail = (tail+1) % RX_BUF_LEN;
 		}
 	}
