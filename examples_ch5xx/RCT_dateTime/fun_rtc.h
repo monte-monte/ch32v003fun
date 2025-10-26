dd78af0af282ce5f8766f5bc6e6a266904dc4042
uni369 <unicabpp@yahoo.com>
2025-10-25 20:46:48 -0400

    remove unused function.

--- examples_ch5xx/RCT_dateTime/fun_rtc.h
+++ examples_ch5xx/RCT_dateTime/fun_rtc.h
@@ -1,24 +1,6 @@
 // MIT License
 // Copyright (c) 2025 UniTheCat
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 #include "ch32fun.h"
 #include <stdio.h>
 
@@ -27,15 +9,6 @@
 //! ####################################
 //! CORE FUNCTION
 //! ####################################
-
-void rtc_print_regs() {
-	UTIL_PRINT_REG8(R8_RTC_FLAG_CTRL, "FLAG_CTRL");
-	UTIL_PRINT_REG8(R8_RTC_MODE_CTRL, "MODE_CTRL");
-	UTIL_PRINT_REG32(R32_RTC_TRIG, "TRIG");
-	UTIL_PRINT_REG16(R16_RTC_CNT_32K, "CNT_32K");
-	UTIL_PRINT_REG16(R16_RTC_CNT_2S, "CNT_2SS");
-	UTIL_PRINT_REG32(R32_RTC_CNT_DAY, "CNT_DAY");
-}
 
 void fun_rtc_printRaw() {
 	printf("\n32k (%d), 2s (%d), day (%ld)\r\n",
