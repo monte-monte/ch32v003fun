30bad4beca6f1eb82f71447a192656a280d64316
uni369 <unicabpp@yahoo.com>
2025-11-09 21:16:49 -0500

    add more comments for the slave.

--- examples_x035/i2c_slave_test/i2c_slave_test.c
+++ examples_x035/i2c_slave_test/i2c_slave_test.c
@@ -1,4 +1,32 @@
-// Simple example demonstrating I2C communication with a BH1750 light sensor
+/**
+ * I2C Slave Communication Example
+ * You can use this with the i2c_sensor_test example
+ * 
+ * 0x3x Slave Command Set:
+ * 0x01 - Minick BH1750 Power on command (returns 1 byte)
+ * 0x23 - Minmick BH1750 Resolution command (returns 1 byte) 
+ * 0x13 - Read 2 bytes from slave
+ * 0x14 - Read 4 bytes from slave
+ * 0x30 - Read from slave's writable buffer (32 bytes)
+ * 0x31 - Write to slave's writable buffer (32 bytes)
+ * 
+ * Command 0x30: Read from writable buffer
+ * Format: { 0x30, start_index }
+ * - start_index: Position to start reading from (0-31)
+ * Since buffer size is 32 bytes (0-31), reading from index 29:
+ * - Returns bytes 29, 30, 31 (3 valid bytes)
+ * - Remaining requested bytes return 0xFF (buffer boundary exceeded)
+ * 
+ * Command 0x31: Write to writable buffer  
+ * Format: { 0x31, start_index, data0, data1, ... }
+ * - start_index: Position to start writing to (0-31)
+ * - dataX: Bytes to write (up to buffer boundary)
+ * Since buffer ends at index 31:
+ * - Writes 0xAA to index 29, 0xBB to index 30, 0xCC to index 31
+ * - 0xDD is not written (buffer full)
+ * - Returns success for written bytes only
+ */
+
 
 #include "ch32fun.h"
 #include <stdio.h>
