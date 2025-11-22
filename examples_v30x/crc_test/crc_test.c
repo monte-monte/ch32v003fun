f2d1abfa19bb5ef0f9cc3640d7a810d282934bbb
uni369 <unicabpp@yahoo.com>
2025-11-22 10:40:01 -0500

    update.

--- examples_v30x/crc_test/crc_test.c
+++ examples_v30x/crc_test/crc_test.c
@@ -2,41 +2,7 @@
 
 #include "ch32fun.h"
 #include <stdio.h>
-
-static void CRC_init(void) {
-	// Enable CRC clock
-	RCC->AHBPCENR |= RCC_AHBPeriph_CRC;
-}
-
-// Calculate CRC for a single 32-bit value
-static u32 CRC_calculate32(u32 data) {
-	// Reset CRC unit
-	CRC->CTLR = CRC_CTLR_RESET;
-	
-	// Write data to trigger CRC calculation
-	CRC->DATAR = data;
-	
-	// Wait for CRC calculation
-	u32 timeout = 10000;
-	while (CRC->DATAR == 0 && --timeout);
-
-	// read the CRC result
-	return CRC->DATAR;
-}
-
-// Calculate CRC for an array of 32-bit values
-static u32 CRC_calculateArray32(u32 *data, u32 length) {
-	CRC->CTLR = CRC_CTLR_RESET;
-	
-	// Process each 32-bit word
-	for(u32 i = 0; i < length; i++) {
-		CRC->DATAR = data[i];
-	}
-	
-	// Return final CRC
-	return CRC->DATAR;
-}
-
+#include "lib_crc.h"
 
 int main() {
 	SystemInit();
