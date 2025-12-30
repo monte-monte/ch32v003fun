85398e6f9e0901bed725c51c7deda77fb0f3c1cb
monte-monte <6649967+monte-monte@users.noreply.github.com>
2025-12-30 08:14:27 +0200

    Added ch5xx support. Added DTR

--- examples_usb/USBFS/usbfs_cdc_uart/uart.h
+++ examples_usb/USBFS/usbfs_cdc_uart/uart.h
@@ -5,64 +5,47 @@
 #include "ch32fun.h"
 
 #ifdef CH5xx
-
-typedef struct
-{
-	uint8_t MCR;
-	uint8_t IER;
-	uint8_t FCR;
-	uint8_t LCR;
-	uint8_t IIR;
-	uint8_t LSR;
-	uint8_t MSR;
+typedef struct {
+	volatile uint8_t MCR;
+	volatile uint8_t IER;
+	volatile uint8_t FCR;
+	volatile uint8_t LCR;
+	volatile uint8_t IIR;
+	volatile uint8_t LSR;
 	uint16_t reserved_1;
-	uint8_t THR;
+	volatile uint8_t THR;
 	uint8_t reserved_2;
-	uint8_t RFC;
-	uint8_t TFC;
-	uint8_t DL;
-	uint8_t reserved_3;
-	uint8_t DIV;
+	volatile uint8_t RFC;
+	volatile uint8_t TFC;
+	volatile uint16_t DL;
+	volatile uint8_t DIV;
+	volatile uint8_t ADR;
 } UART_TypeDef;
 
-#define USART_WordLength_5b ((uint8_t)0x00)
-#define USART_WordLength_6b ((uint8_t)0x01)
-#define USART_WordLength_7b ((uint8_t)0x02)
-#define USART_WordLength_8b ((uint8_t)0x03)
-#define USART_StopBits_1    ((uint8_t)0x00)
-#define USART_StopBits_2    ((uint8_t)0x04)
-#define USART_Parity_No     ((uint8_t)0x00)
-#define USART_Parity_Odd    ((uint8_t)0x08)
-#define USART_Parity_Even   ((uint8_t)0x18)
-#define USART_Parity_Mark   ((uint8_t)0x28)
-#define USART_Parity_Space  ((uint8_t)0x38)
+#define USART_WordLength_5b 0x00
+#define USART_WordLength_6b 0x01
+#define USART_WordLength_7b 0x02
+#define USART_WordLength_8b 0x03
+#define USART_StopBits_1    0x00
+#define USART_StopBits_2    0x04
+#define USART_Parity_No     0x00
+#define USART_Parity_Odd    0x08
+#define USART_Parity_Even   0x18
+#define USART_Parity_Mark   0x28
+#define USART_Parity_Space  0x38
 
-typedef enum {
-	UART_WORDL_5 = 0,
-	UART_WORDL_6 = 1,
-	UART_WORDL_7 = 2,
-	UART_WORDL_8 = 3,
-} UART_WordLength;
-
-typedef enum {
-	UART_STOP_1 = 0,
-	UART_STOP_2 = 4,
-} USART_StopBits_1
-
-#define UART_BASE_ADDR(n) 0x40003000+(0x400*n)
-#define UART(n)           ((UART_TypeDef *)UART_BASE_ADDR(n))
+#define UART_BASE_ADDR(n)   ((uint32_t)(0x40003000+(0x400*n)))
+#define UART(n)             ((UART_TypeDef *)UART_BASE_ADDR(n))
 
 #else
 
-#define UART_BASE_ADDR(n) USART2_BASE+(0x400*(n-2))
-#define UART(n)           ((USART_TypeDef *)UART_BASE_ADDR(n))
-#define DMA_BASE_ADDR(n)  (DMA1_Channel1_BASE+(0x14*(n-1)))
-#define DMA1_CH(n)        ((DMA_Channel_TypeDef *)DMA_BASE_ADDR(n))
+#define UART_BASE_ADDR(n)   USART2_BASE+(0x400*(n-2))
+#define UART(n)             ((USART_TypeDef *)UART_BASE_ADDR(n))
+#define DMA_BASE_ADDR(n)    (DMA1_Channel1_BASE+(0x14*(n-1)))
+#define DMA1_CH(n)          ((DMA_Channel_TypeDef *)DMA_BASE_ADDR(n))
 
-#define UART_TX_DMA DMA1_CH(7)
-#define UART_RX_DMA DMA1_CH(6)
-// #define UART_TX_DMA DMA1_Channel7
-// #define UART_RX_DMA DMA1_Channel6
+#define UART_TX_DMA         DMA1_CH(7)
+#define UART_RX_DMA         DMA1_CH(6)
 
 /*** Macro Functions by ADBeta*********************************************************/
 // DIV  = round( (HCLK / (16 * BAUD)) * 16 )
@@ -72,22 +55,28 @@ typedef enum {
 // Simplify equation and add Divisor/2 to simulate intager rounding
 #define UART_CALC_BAUD(DIV) (((FUNCONF_SYSTEM_CORE_CLOCK) + ((DIV) / 2)) / (DIV))
 /***************************************************************************************/
+#define UART_DEFAULT_FLOW   USART_HardwareFlowControl_None
 
 #endif
-#define UART_TX_BUF_SIZE 1024
-#define UART_RX_BUF_SIZE 2048
-#define UART_USB_TIMEOUT 600
-#define UART_ZERO_TIMEOUT 60
-#define UART_RX_TIMEOUT 3
+
+#define UART_TX_BUF_SIZE    1024
+#define UART_RX_BUF_SIZE    2048
+#define UART_USB_TIMEOUT    600
+#define UART_ZERO_TIMEOUT   60
+#define UART_RX_TIMEOUT     3
 
 #ifndef UART_NUMBER
-#define UART_NUMBER 2
+#if defined(CH5xx)
+#define UART_NUMBER         1
+#else
+#define UART_NUMBER         2
 #endif
-#define UART_DEFALT_BAUD 115200
-#define UART_DEFAULT_WORDL USART_WordLength_8b
-#define UART_DEFAULT_STOPB USART_StopBits_1
+#endif
+
+#define UART_DEFALT_BAUD    115200
+#define UART_DEFAULT_WORDL  USART_WordLength_8b
+#define UART_DEFAULT_STOPB  USART_StopBits_1
 #define UART_DEFAULT_PARITY USART_Parity_No
-#define UART_DEFAULT_FLOW USART_HardwareFlowControl_None
 
 __attribute__ ((aligned(4))) uint8_t uart_tx_buffer[UART_TX_BUF_SIZE];
 __attribute__ ((aligned(4))) uint8_t uart_rx_buffer[UART_RX_BUF_SIZE];
